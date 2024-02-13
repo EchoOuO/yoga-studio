e4a5c10e935b8fc5c16c1f202407a1df18cffd96
EchoOuO <window9995@gmail.com>
2024-02-13 00:43:02 -0800

    1. Hamburger menu done 2. Working on animation, hover and other cool effects

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// Light/Dark mode transition
+
 let main = document.querySelector("main");
 let modeTransition = document.querySelector(".nav-mode-button");
 
@@ -10,3 +12,30 @@ modeTransition.addEventListener("click", function () {
     modeTransition.src = "./assets/moon.png";
   }
 });
+
+// Hamburger menu pop-out
+
+let hamburgerMenuButton = document.querySelector(".nav-menu-button");
+let hamburgerMenuButtonCircle = document.querySelectorAll(".menu-circle");
+console.log(hamburgerMenuButtonCircle);
+let hamburgerMenu = document.querySelector(".hamburger-menu");
+let hamburgerMenuButtonClose = document.querySelector(".menu-cross");
+
+hamburgerMenuButton.addEventListener("click", function () {
+  hamburgerMenu.classList.toggle("hamburger-menu-shift");
+  hamburgerMenuButtonClose.classList.toggle("hidden");
+});
+
+for (let i = 0; i < hamburgerMenuButtonCircle.length; i++) {
+  hamburgerMenuButton.addEventListener("click", function () {
+    hamburgerMenuButtonCircle[i].classList.toggle("hidden");
+  });
+}
+
+// replace opening animation by adding ID with another animation
+
+let headerImgAnimation = document.querySelector("#header-img-close");
+
+setTimeout(() => {
+  headerImgAnimation.id = "header-img";
+}, 2000);
